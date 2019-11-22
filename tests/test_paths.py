0e887c1bc1b586a0bb1ae63b55b197a503e909dd
tycarac <>
2019-11-22 13:14:30 +1100

    Simple path utility functions

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,6 +1,6 @@
 import logging
-
-from toolbox import paths
+from pathlib import Path
+from toolbox import paths as p
 
 logger = logging.getLogger(__name__)
 
@@ -9,11 +9,24 @@ logger = logging.getLogger(__name__)
 def test_path_unicode():
     given = 'AWS Well-Architected Framework \u2013 Operational Excellence Pillar'
     expected = 'AWS Well-Architected Framework - Operational Excellence Pillar'
-    assert paths.sanitize_filename(given) == expected
+    assert p.sanitize_filename(given) == expected
 
 
 # _____________________________________________________________________________
 def test_path_bad_characters():
     given = 'WordPress: Best Practices on AWS'
     expected = 'WordPress- Best Practices on AWS'
-    assert paths.sanitize_filename(given) == expected
+    assert p.sanitize_filename(given) == expected
+
+
+# _____________________________________________________________________________
+def test_is_parent_path():
+    tests = [
+        [Path('c:\\atemp'), Path('c:\\atemp'), True],
+        [Path('c:\\atemp'), Path('c:\\atemp\\abcd'), True],
+        [Path('c:\\abcd\\abcd'), Path('c:\\temp\\abcd'), False]
+    ]
+
+    for test in tests:
+        p1, p2, result = test
+        assert p.is_parent(p1, p2) == result
--- toolbox/paths.py
+++ toolbox/paths.py
@@ -1,9 +1,40 @@
+import os
+from pathlib import Path
 import unicodedata
 
 # https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
 WINDOWS_INVALID_FILENAME_CHARS = ['\\', '/', ':', '*', '|', '?', '>', '<', '"']
 DASH_CHARS = ['\u2012', '\u2013', '\u2014', '\u2015', '\u2053']
 REPLACE_CHARS = WINDOWS_INVALID_FILENAME_CHARS + DASH_CHARS
+
+
+# _____________________________________________________________________________
+def is_parent(parent: Path, path: Path):
+    """Returns True is path has the same parent path as parent
+    :param parent:
+    :param path:
+    :return: True if parent path is contained in path
+    """
+    parent = parent.resolve()
+    path = path.resolve()
+    return str(path).startswith(str(parent))
+
+
+# _____________________________________________________________________________
+def __delete_empty_directories(folder):
+    """Deletes all empty child folders under a parent folder
+    :param folder: parent folder
+    :return: List of deleted folders
+    """
+    deleted_folders = []
+    for root, dirs, _ in os.walk(folder, topdown=False):
+        for dir in dirs:
+            name = os.path.join(root, dir)
+            if not len(os.listdir(name)):
+                deleted_folders.append(dir)
+                os.rmdir(name)
+
+    return deleted_folders
 
 
 # _____________________________________________________________________________
