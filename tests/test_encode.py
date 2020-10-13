f1b171eadb803e11b4d75477b3b462027a7ce122
tycarac <>
2020-10-13 23:05:26 +1100

    Added character set encoding

---
+++ tests/test_encode.py
@@ -0,0 +1,42 @@
+import logging
+import pytest
+
+
+from toolbox.encode import CharSetEncoder
+
+_logger = logging.getLogger(__name__)
+
+
+# _____________________________________________________________________________
+@pytest.mark.parametrize('given, expected', [
+    ('', '0fh43w'),
+    ('1', 'jtvky4'),
+    ('2', '0s5mgf'),
+    ('https://docs.aws.amazon.com', 'zfv3pv2'),
+    ('https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-specification.html/serverless-application-model-updates.rss', '6wdkr6')
+    ])
+def test_hash(given, expected):
+    encoder = CharSetEncoder()
+    assert encoder.hash(given) == expected
+
+
+# _____________________________________________________________________________
+@pytest.mark.parametrize('given, expected', [
+    ('', '3vkzzbpyxf'),
+    ('1', 'fnkt2wfzg'),
+    ('2', '4rvs04xqg9'),
+    ('https://docs.aws.amazon.com', 'jzmjqfmdc'),
+    ('https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-specification.html/serverless-application-model-updates.rss', 'hhh6jr8858')
+    ])
+def test_hash_6(given, expected):
+    encoder = CharSetEncoder(digest_size=6)
+    assert encoder.hash(given) == expected
+
+
+# _____________________________________________________________________________
+@pytest.mark.parametrize('given, expected', [
+    (1, 1),
+])
+def test_encode_decode(given, expected):
+    encoder = CharSetEncoder()
+    assert encoder.decode(encoder.encode(given)) == expected
---
+++ toolbox/encode.py
@@ -0,0 +1,31 @@
+from hashlib import blake2b
+from sys import byteorder
+
+
+# _____________________________________________________________________________
+class CharSetEncoder:
+    _char_set = '0123456789bcdfghjkmnpqrstvwxyz'
+
+    def __init__(self, digest_size=4, char_set=None):
+        self._char_set = char_set if char_set else CharSetEncoder._char_set
+        self._digest_size = digest_size
+        self._base = len(self._char_set)
+
+    def hash(self, string: str) -> str:
+        return self.encode(int.from_bytes(blake2b(bytearray(string, 'utf-8'), digest_size=self._digest_size).digest(),
+                                          byteorder=byteorder))
+
+    def encode(self, number: int) -> str:
+        if number == 0:
+            return self._char_set[0]
+        string = ''
+        while number != 0:
+            number, rem = divmod(number, self._base)
+            string += self._char_set[rem]
+        return string
+
+    def decode(self, string: str) -> int:
+        number = 0
+        for ch in string[::-1]:
+            number = number * self._base + self._char_set.index(ch)
+        return number
