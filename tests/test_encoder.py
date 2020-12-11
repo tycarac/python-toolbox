4a6246fd5aa0c3ab83943f2ba0e11d6bc800443a
tycarac <>
2020-12-11 20:48:44 +1100

    Expanded test cases

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -10,8 +10,11 @@ _logger = logging.getLogger(__name__)
 # _____________________________________________________________________________
 @pytest.mark.parametrize('given, expected', [
     ('', '0fh43w'),
+    ('0', '0t1g7v'),
     ('1', 'jtvky4'),
     ('2', '0s5mgf'),
+    ('a', '85zqwy1'),
+    ('https://', 'pq4px7'),
     ('https://docs.aws.amazon.com', 'zfv3pv2'),
     ('https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-specification.html/serverless-application-model-updates.rss', '6wdkr6')
     ])
@@ -23,8 +26,11 @@ def test_hash_4(given, expected):
 # _____________________________________________________________________________
 @pytest.mark.parametrize('given, expected', [
     ('', '3vkzzbpyxf'),
+    ('0', 'mjts0v6qh'),
     ('1', 'fnkt2wfzg'),
     ('2', '4rvs04xqg9'),
+    ('a', 'kzx4j8w9s'),
+    ('https://', 'rkvqr30203'),
     ('https://docs.aws.amazon.com', 'jzmjqfmdc'),
     ('https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-specification.html/serverless-application-model-updates.rss', 'hhh6jr8858')
     ])
@@ -35,7 +41,12 @@ def test_hash(given, expected):
 
 # _____________________________________________________________________________
 @pytest.mark.parametrize('given, expected', [
+    (0, 0),
     (1, 1),
+    (11, 11),
+    (111, 111),
+    (991, 991),
+    (999999999991, 999999999991),
 ])
 def test_encode_decode(given, expected):
     assert CharSetEncoder.decode(CharSetEncoder.encode(given)) == expected
