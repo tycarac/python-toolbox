1b31d33747d0826631077da9d88941906a76e9d6
tycarac <>
2024-10-19 23:16:09 +1100

    Minor code cleanup

--- toolbox/counters.py
+++ toolbox/counters.py
@@ -28,20 +28,23 @@ class IncLetterCounter(object):
     _arr: List[int]
     _value: str
 
-    def __init__(self, init_value: str='A'):
+    def __init__(self, init_value: str = 'A'):
         self._value = init_value
         self._arr = list(ord(c) for c in reversed(init_value))
 
     def inc(self) -> None:
         self._arr[0] += 1
-        for i in range(len(self._arr)):
-            if self._arr[i] > 90:
-                self._arr[i] = 65
-                if i < len(self._arr) - 1:
+        if (l := len(self._arr)) == 1 and (c := self._arr[0]) <= 90:
+            self._value = chr(c)
+        else:
+            for i in range(l - 1):
+                if self._arr[i] > 90:
+                    self._arr[i] = 65
                     self._arr[i + 1] += 1
-                else:
-                    self._arr.append(65)
-        self._value = ''.join(chr(x) for x in reversed(self._arr))
+            if self._arr[-1] > 90:
+                self._arr[-1] = 65
+                self._arr.append(65)
+            self._value = ''.join(chr(x) for x in reversed(self._arr))
 
     @property
     def value(self) -> str:
