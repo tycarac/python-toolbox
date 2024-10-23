9bcd12341b08eab56c93a73f38cd04b6ddd1f30d
tycarac <>
2024-10-23 18:18:20 +1100

    Added Letter Number Counter

--- tests/test_counters.py
+++ tests/test_counters.py
@@ -1,6 +1,6 @@
 import logging.config
 
-from toolbox.counters import IncCounter, IncLetterCounter
+from toolbox.counters import IncCounter, IncLetterCounter, IncLetterNumberCounter
 
 _logger = logging.getLogger(__name__)
 
@@ -64,3 +64,34 @@ def test_inc_letter_counter():
     assert ctr.inc_value == 'CAA'
     assert ctr.inc_value == 'CAB'
     assert ctr.inc_value == 'CAC'
+
+
+# _____________________________________________________________________________
+def test_inc_number_letter_counter():
+    ctr = IncLetterNumberCounter()
+    assert ctr.value == 'A1'
+    assert ctr.inc_number_value == 'A2'
+    assert ctr.value == 'A2'
+    assert ctr.inc_number_value == 'A3'
+    assert ctr.value == 'A3'
+    assert ctr.inc_number_value == 'A4'
+
+    assert ctr.inc_letter_value == 'B4'
+    assert ctr.value == 'B4'
+    assert ctr.inc_letter_value == 'C4'
+    assert ctr.value == 'C4'
+    assert ctr.inc_letter_value == 'D4'
+
+    assert ctr.inc_number_value == 'D5'
+    assert ctr.inc_letter_value == 'E5'
+    assert ctr.inc_number_value == 'E6'
+    assert ctr.inc_letter_value == 'F6'
+    assert ctr.inc_number_value == 'F7'
+
+    ctr = IncLetterNumberCounter('Y8')
+    assert ctr.inc_number_value == 'Y9'
+    assert ctr.inc_letter_value == 'Z9'
+    assert ctr.inc_number_value == 'Z10'
+    assert ctr.inc_letter_value == 'AA10'
+    assert ctr.inc_number_value == 'AA11'
+    assert ctr.inc_letter_value == 'AB11'
--- toolbox/counters.py
+++ toolbox/counters.py
@@ -1,6 +1,8 @@
 import multiprocessing
 from typing import List
+import re
 
+re_letter_number = re.compile(r'\s*([A-Z]+)([0-9]+)\s*')
 
 # _____________________________________________________________________________
 class IncCounter(object):
@@ -29,8 +31,8 @@ class IncLetterCounter(object):
     _value: str
 
     def __init__(self, init_value: str = 'A'):
-        self._value = init_value
-        self._arr = list(ord(c) for c in reversed(init_value))
+        self._value = init_value.upper()
+        self._arr = list(ord(c) for c in reversed(self._value))
 
     def inc(self) -> None:
         self._arr[0] += 1
@@ -54,3 +56,46 @@ class IncLetterCounter(object):
     def inc_value(self) -> str:
         self.inc()
         return self._value
+
+# _____________________________________________________________________________
+class IncLetterNumberCounter(object):
+    _arr: List[int]
+    _number: int
+    _str : str
+    _value: str
+
+    def __init__(self, init_value: str = 'A1'):
+        if not (matches := re_letter_number.match(init_value.upper())):
+            raise RuntimeError(f'Not valid input: "{init_value}"')
+        self._str = matches[1]
+        self._number = int(matches[2])
+        self._value = self._str + str(self._number)
+        self._arr = list(ord(c) for c in reversed(self._str))
+
+    @property
+    def inc_letter_value(self) -> str:
+        self._arr[0] += 1
+        if (l := len(self._arr)) == 1 and (c := self._arr[0]) <= 90:
+            self._str = chr(c)
+        else:
+            for i in range(l - 1):
+                if self._arr[i] > 90:
+                    self._arr[i] = 65
+                    self._arr[i + 1] += 1
+            if self._arr[-1] > 90:
+                self._arr[-1] = 65
+                self._arr.append(65)
+            self._str = ''.join(chr(x) for x in reversed(self._arr))
+        self._value = self._str + str(self._number)
+        return self._value
+
+    @property
+    def inc_number_value(self) -> str:
+        self._number += 1
+        self._value = self._str + str(self._number)
+        return self._value
+
+    @property
+    def value(self) -> str:
+        return self._value
+
